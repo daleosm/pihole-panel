7d88c7bb708c2652073ede1aa1d7f55b13da1f76
Dale Osm <40340217+daleosm@users.noreply.github.com>
2018-07-08 22:12:07 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -21,18 +21,18 @@ class GridWindow(Gtk.Window):
         self.add(grid)
 
         button1 = Gtk.Button(label="Button 1")
-        button2 = Gtk.Label(label="Blocked Today:" + str(json_obj['ads_blocked_today']), angle=0, halign=Gtk.Align.END)
+        button2 = Gtk.Label(label="Blocked Today: " + str(json_obj['ads_blocked_today']), angle=0, halign=Gtk.Align.END)
         button3 = Gtk.Button(label="Button 3")
-        button4 = Gtk.Button(label="Button 4")
-        button5 = Gtk.Button(label="Button 5")
-        button6 = Gtk.Button(label="Button 6")
+        button4 = Gtk.Label(label="DNS Queries Today: " + str(json_obj['dns_queries_today']), angle=0, halign=Gtk.Align.END)
+        button5 = Gtk.Label(label="ADS Percentage Today: " + str(json_obj['ads_percentage_today']), angle=0, halign=Gtk.Align.END)
+        #button6 = Gtk.Button(label="Button 6")
 
         grid.add(button1)
         grid.attach(button2, 1, 0, 2, 1)
         grid.attach_next_to(button3, button1, Gtk.PositionType.BOTTOM, 1, 2)
         grid.attach_next_to(button4, button3, Gtk.PositionType.RIGHT, 2, 1)
         grid.attach(button5, 1, 2, 1, 1)
-        grid.attach_next_to(button6, button5, Gtk.PositionType.RIGHT, 1, 1)
+        #grid.attach_next_to(button5, Gtk.PositionType.RIGHT, 1, 1)
 
 win = GridWindow()
 win.connect("destroy", Gtk.main_quit)
