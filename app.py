d51afdb875b256e16cb6a66f7411ffc1078d12e1
Dale Osm <40340217+daleosm@users.noreply.github.com>
2018-07-10 19:19:51 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -20,19 +20,24 @@ class GridWindow(Gtk.Window):
         grid = Gtk.Grid()
         self.add(grid)
 
-        button1 = Gtk.Button(label="Button 1")
-        button2 = Gtk.Label(label="Blocked Today: " + str(json_obj['ads_blocked_today']), angle=0, halign=Gtk.Align.END)
-        button3 = Gtk.Button(label="Button 3")
-        button4 = Gtk.Label(label="DNS Queries Today: " + str(json_obj['dns_queries_today']), angle=0, halign=Gtk.Align.END)
-        button5 = Gtk.Label(label="ADS Percentage Today: " + str(json_obj['ads_percentage_today']), angle=0, halign=Gtk.Align.END)
-        #button6 = Gtk.Button(label="Button 6")
+        frame_vert = Gtk.Frame(label='Pi-hole Stats')
 
-        grid.add(button1)
-        grid.attach(button2, 1, 0, 2, 1)
-        grid.attach_next_to(button3, button1, Gtk.PositionType.BOTTOM, 1, 2)
-        grid.attach_next_to(button4, button3, Gtk.PositionType.RIGHT, 2, 1)
-        grid.attach(button5, 1, 2, 1, 1)
-        #grid.attach_next_to(button5, Gtk.PositionType.RIGHT, 1, 1)
+        button1 = Gtk.Switch()
+        #button1.connect("notify::active", self.on_switch_activated)
+        #button1.set_active(True)
+
+        box = Gtk.Box(spacing=6)
+
+        stats = Gtk.Label(label="Blocked Today: " + str(json_obj['ads_blocked_today']) + "\n" +
+        "Queries Today: " + str(json_obj['dns_queries_today']) + "\n"
+        "Percentage Blckd: " + str(json_obj['ads_percentage_today']) + "%", halign=Gtk.Align.START)
+        box.pack_start(stats, True, True, 0)
+
+        frame_vert.add(box)
+
+        grid.attach(button1, 4, 1, 1, 1)
+        grid.attach(frame_vert, 1, 2, 1, 1)
+
 
 win = GridWindow()
 win.connect("destroy", Gtk.main_quit)
