f516177b300e79f0c5ee5d944a615dc7a795a6f4
Dale Osm <40340217+daleosm@users.noreply.github.com>
2018-07-15 18:58:45 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 import gi
 import json
+from urllib.request import urlopen
+
+# If you have UTF-8 problems then uncomment next 3 lines
+#import sys
+#reload(sys)
+#sys.setdefaultencoding("utf-8")
 
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-import urllib.request
-url = "http://192.168.0.12/admin/api.php"
-resp = urllib.request.urlopen(url).read().decode("UTF-8")
-json_obj = json.loads(resp)
+# Change to Your Pi-Hole Admin Console Url
+pihole = "http://pi.hole/admin/"
+# Change to Your Pi-Hole Admin Console Hashed Password (see WEBPASSWORD in /etc/pihole/setupVars.conf)
+passwd = ""
 
 class GridWindow(Gtk.Window):
 
     def __init__(self):
         Gtk.Window.__init__(self, title="PiHole-Panel")
 
-        print (json_obj)
-        print(json_obj['ads_blocked_today'])
+        url = pihole + "api.php?summary"
+        result = urlopen(url, timeout = 15).read()
+        json_obj = json.loads(result)
 
         grid = Gtk.Grid(margin=10)
         grid.set_column_homogeneous(True)
