870c3fb55f019ff0966dc844293c26133b17501a
Dale Osm <dale.osmond@gmx.com>
2018-08-24 21:19:15 +0100

    Set installer to cleanup after install

--- setup.sh
+++ setup.sh
@@ -8,10 +8,12 @@ fi
 cd /tmp
 $SUDO wget https://github.com/daleosm/PiHole-Panel/archive/master.zip
 $SUDO unzip master.zip
+$SUDO rm master.zip
 
 cd PiHole-Panel-master/
 $SUDO mv bin/pihole-panel /usr/bin
 $SUDO mv pihole-panel /usr/lib
 $SUDO mv pihole-panel.desktop /usr/share/applications
+$SUDO rm -R /tmp/PiHole-Panel-master
 
 echo "Done!"
