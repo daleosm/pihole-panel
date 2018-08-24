664002694d12cdb5b114f4f662da0d5f243bfae0
Dale Osm <dale.osmond@gmx.com>
2018-08-24 20:58:12 +0100

    Setup path fix

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ cd /tmp
 $SUDO wget https://github.com/daleosm/PiHole-Panel/archive/master.zip
 $SUDO unzip master.zip
 
-cd master/PiHole-Panel-master/
+cd PiHole-Panel-master/
 $SUDO mv bin/pihole-panel /usr/bin
 $SUDO mv pihole-panel /usr/lib
 $SUDO mv pihole-panel.desktop /usr/share/applications
