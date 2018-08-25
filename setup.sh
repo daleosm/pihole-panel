548e2d00d4bfa8d8c6aa9e07ee00a06a81a038e6
Dale Osm <dale.osmond@gmx.com>
2018-08-25 18:21:21 +0100

    Fixed !=: not found error

--- setup.sh
+++ setup.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-SUDO=''
-if (( $EUID != 0 )); then
-    SUDO='sudo'
-fi
-
 cd /tmp
-$SUDO wget https://github.com/daleosm/PiHole-Panel/archive/master.zip
-$SUDO unzip master.zip
-$SUDO rm master.zip
+wget https://github.com/daleosm/PiHole-Panel/archive/master.zip
+unzip master.zip
+rm master.zip
 
 cd PiHole-Panel-master/
-$SUDO mv -f bin/pihole-panel /usr/bin
-$SUDO rm -R /usr/lib/pihole-panel
-$SUDO mv pihole-panel /usr/lib
-$SUDO mv -f pihole-panel.desktop /usr/share/applications
-$SUDO rm -R /tmp/PiHole-Panel-master
+mv -f bin/pihole-panel /usr/bin
+rm -R /usr/lib/pihole-panel
+mv pihole-panel /usr/lib
+mv -f pihole-panel.desktop /usr/share/applications
+rm -R /tmp/PiHole-Panel-master
 
 echo "Done!"
